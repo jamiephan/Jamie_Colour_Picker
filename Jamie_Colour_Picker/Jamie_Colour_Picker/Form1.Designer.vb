3151dc0919203215545455ea962d25d7304e42bb
Jamie Phan <jamie_phan@hotmail.com>
2014-12-08 20:42:45 +0000

    First Publish
    First Publish

---
+++ Jamie_Colour_Picker/Jamie_Colour_Picker/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtboxHex = New System.Windows.Forms.TextBox()
+        Me.txtBoxRed = New System.Windows.Forms.TextBox()
+        Me.txtboxGreen = New System.Windows.Forms.TextBox()
+        Me.txtboxBlue = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Red
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Location = New System.Drawing.Point(2, 2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(168, 50)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Hex Value :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(34, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Red :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 110)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Green :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(34, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Blue :"
+        '
+        'txtboxHex
+        '
+        Me.txtboxHex.Location = New System.Drawing.Point(67, 55)
+        Me.txtboxHex.Name = "txtboxHex"
+        Me.txtboxHex.ReadOnly = True
+        Me.txtboxHex.Size = New System.Drawing.Size(100, 20)
+        Me.txtboxHex.TabIndex = 1
+        Me.txtboxHex.Text = "#FF0000"
+        '
+        'txtBoxRed
+        '
+        Me.txtBoxRed.Location = New System.Drawing.Point(67, 81)
+        Me.txtBoxRed.Name = "txtBoxRed"
+        Me.txtBoxRed.ReadOnly = True
+        Me.txtBoxRed.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxRed.TabIndex = 2
+        Me.txtBoxRed.Text = "255"
+        '
+        'txtboxGreen
+        '
+        Me.txtboxGreen.Location = New System.Drawing.Point(67, 107)
+        Me.txtboxGreen.Name = "txtboxGreen"
+        Me.txtboxGreen.ReadOnly = True
+        Me.txtboxGreen.Size = New System.Drawing.Size(100, 20)
+        Me.txtboxGreen.TabIndex = 3
+        Me.txtboxGreen.Text = "0"
+        '
+        'txtboxBlue
+        '
+        Me.txtboxBlue.Location = New System.Drawing.Point(67, 133)
+        Me.txtboxBlue.Name = "txtboxBlue"
+        Me.txtboxBlue.ReadOnly = True
+        Me.txtboxBlue.Size = New System.Drawing.Size(100, 20)
+        Me.txtboxBlue.TabIndex = 4
+        Me.txtboxBlue.Text = "0"
+        '
+        'Timer1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(95, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "stop"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(2, 143)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox1.ShortcutsEnabled = False
+        Me.RichTextBox1.Size = New System.Drawing.Size(10, 10)
+        Me.RichTextBox1.TabIndex = 11
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.Visible = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 750
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(172, 158)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtboxBlue)
+        Me.Controls.Add(Me.txtboxGreen)
+        Me.Controls.Add(Me.txtBoxRed)
+        Me.Controls.Add(Me.txtboxHex)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Jamie Colour"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ColorDialog1 As System.Windows.Forms.ColorDialog
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtboxHex As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxRed As System.Windows.Forms.TextBox
+    Friend WithEvents txtboxGreen As System.Windows.Forms.TextBox
+    Friend WithEvents txtboxBlue As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
